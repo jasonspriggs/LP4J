18e4e74bf8c1e6aa94eeee11fb490e16e35d269c
Jason Spriggs <jason@jasonspriggs.com>
2017-10-22 19:06:31 -0400

    Lights

--- src/com/jasonspriggs/lp4jmk2/Runner.java
+++ src/com/jasonspriggs/lp4jmk2/Runner.java
@@ -1,5 +1,10 @@
 package com.jasonspriggs.lp4jmk2;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
 import net.thecodersbreakfast.lp4j.api.*;
@@ -8,24 +13,31 @@ import net.thecodersbreakfast.lp4j.midi.*;
 public class Runner {
 
     private static CountDownLatch stop = new CountDownLatch(1);
+    private static String ip;
 
     public static void main(String[] args) throws Exception {
-
     	Launchpad launchpad = new MidiLaunchpad(MidiDeviceConfiguration.autodetect());
-        LaunchpadClient client = launchpad.getClient();
+	    LaunchpadClient client = launchpad.getClient();
+	
+	    MyListener myListener = new MyListener(client);
+	    launchpad.setListener(myListener);
+	    
+	    for(int y = 7; y >= 0; y--) {
+	    	for(int x = 0; x <= 7; x++) {
+	    		client.setPadLight(Pad.at(x, y), Color.BLACK, BackBufferOperation.NONE);
+	    	}
+			client.setButtonLight(Button.atTop(y), Color.BLACK, BackBufferOperation.NONE);
+			client.setButtonLight(Button.atRight(y), Color.BLACK, BackBufferOperation.NONE);
+	    }
 
-        MyListener myListener = new MyListener(client);
-        launchpad.setListener(myListener);
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Light IP: ");
+    	ip = sc.next();
+    	HttpURLConnection con1 = (HttpURLConnection) new URL("http://" + ip + "/clear").openConnection();
+    	con1.setRequestMethod("GET");
         
-        for(int y = 7; y >= 0; y--) {
-        	for(int x = 0; x <= 7; x++) {
-        		client.setPadLight(Pad.at(x, y), Color.BLACK, BackBufferOperation.NONE);
-        	}
-    		client.setButtonLight(Button.atTop(y), Color.BLACK, BackBufferOperation.NONE);
-    		client.setButtonLight(Button.atRight(y), Color.BLACK, BackBufferOperation.NONE);
-        }
-        
-        //client.setButtonLight(Button.atTop(y), Color.BLACK, BackBufferOperation.NONE);
+        if(con1.getResponseCode() == 200)
+	        client.setButtonLight(Button.SESSION, Color.GREEN, BackBufferOperation.NONE);
         
         
         
@@ -45,14 +57,12 @@ public class Runner {
         @Override
         public void onPadReleased(Pad pad, long timestamp) {
             System.out.println(pad);
-            //client.setPadLight(pad, Color.YELLOW, BackBufferOperation.CLEAR);
             
         }
 
         @Override
         public void onButtonReleased(Button button, long timestamp) {
         	System.out.println(button);
-            //client.setButtonLight(button, Color.AMBER, BackBufferOperation.CLEAR);
             if(button == Button.SND_A) {
             	int c = 0;
                 for(int y = 7; y >= 0; y--) {
@@ -60,8 +70,6 @@ public class Runner {
                 		client.setPadLight(Pad.at(x, y), Color.of(c), BackBufferOperation.NONE);
                 		c++;
                 	}
-            		client.setButtonLight(Button.SND_A, Color.of(3), BackBufferOperation.NONE);
-            		client.setButtonLight(Button.SND_B, Color.BLACK, BackBufferOperation.NONE);
                 }
             } else if(button == Button.SND_B) {
             	int c = 64;
@@ -70,9 +78,22 @@ public class Runner {
                 		client.setPadLight(Pad.at(x, y), Color.of(c), BackBufferOperation.NONE);
                 		c++;
                 	}
-            		client.setButtonLight(Button.SND_A, Color.BLACK, BackBufferOperation.NONE);
-            		client.setButtonLight(Button.SND_B, Color.of(3), BackBufferOperation.NONE);
                 }
+            } else if(button == Button.STOP) {
+                for(int y = 7; y >= 0; y--) {
+                	for(int x = 0; x <= 7; x++) {
+                		client.setPadLight(Pad.at(x, y), Color.BLACK, BackBufferOperation.NONE);
+                	}
+                }
+                
+            	HttpURLConnection con2;
+				try {
+					con2 = (HttpURLConnection) new URL("http://" + ip + "/add_saved_animation/Off").openConnection();
+	            	con2.setRequestMethod("POST");
+	            	con2.getResponseCode();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
             }
         }
     }
